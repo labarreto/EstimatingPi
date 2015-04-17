ddf27f536454a69c50137be464813c1896b73492
Laura Barreto <labarreto@vassar.edu>
2015-04-17 02:25:47 -0400

    trying to work on simulating the estimation of pi

--- src/estimatingpi/EstimatingPi.java
+++ src/estimatingpi/EstimatingPi.java
@@ -8,52 +8,103 @@ package estimatingpi;
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
+
 /**
  *
  * @author ldbruby95
  */
-public class EstimatingPi extends JApplet implements Runnable{
-    
+public class EstimatingPi extends JApplet implements Runnable {
 
-    
+
+    int center;
+    int pin;
+    double pi;
+    static Dimension d = new Dimension(750,750);
+    static Graphics gC; // circle
+    static Graphics gS; // square
+    static Graphics point;
+
     public static void Go(double r, int n) {
         //r is radius
         //n is number of points to be made
         int count = 0;
         double I;
-        double AoS = (2.0*r) * (2.0*r); // area of square;
+        double AoS = (2.0 * r) * (2.0 * r); // area of square;
         double pi;
+
+        int pW = 1;
+        int pH = 1;
+
+
         
+        int cornerX = 0; // top corner to be at 0,0
+        int cornerY = 0; // top corner to be at 0,0
+        // these will have problems with loosing significance. 
+        int width = (int) r*2; 
+        int height = (int) r*2;
+        
+        Color color = Color.magenta;
+        
+        
+        gC.drawOval(cornerX, cornerY, width, height);
+        gS.drawRect(cornerX, cornerY, width, height);
+        
+        
+        
+        JPanel panel = new JPanel();
+
+        //creating frame
         JFrame f = new JFrame();
+        f.setSize(d);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         Random rd = new Random();
-            
+
         int m = 0;
+        
         for (int i = 0; i < n; i++) {
             m++;
             // calculating pi, how many have we done so far
-            double x = rd.nextDouble()*2*r - r;
-            double y = rd.nextDouble()*2*r - r; 
-            if (inCircleHuh(x,y,r)) {
+            double x = rd.nextDouble() * 2 * r - r;
+            double y = rd.nextDouble() * 2 * r - r;
+            if (inCircleHuh(x, y, r)) {
+
+                color = Color.BLUE;
+
                 count++;
+                point.drawOval((int) x, (int) y, pW, pH);
+                
             }
-            I = AoS * ((double) count / (double) m);        
-            pi = I/(r*r);       
+            point.setColor(color);
+            I = AoS * ((double) count / (double) m);
+            pi = I / (r * r);
             System.out.println(pi);
-            
+
+            JLabel label = new JLabel("Count: " + count + ", Pi estimate: " + pi);
         }
-        
+
     }
-    
+
     public static boolean inCircleHuh(double x, double y, double r) {
         //System.out.println("x = " + x + ", y = " + y);
-        return (Math.sqrt(x * x + y * y)) <= (r); 
+        return (Math.sqrt(x * x + y * y)) <= (r);
     }
-    
-   
+
+    public static void drawCircle(Graphics g, int x, int y, int radius) {
+
+        int diameter = radius * 2;
+
+        //shift x and y by the radius of the circle in order to correctly center it
+        g.fillOval(x - radius, y - radius, diameter, diameter);
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
     public static void main(String[] args) {
-        Go(1.0,1000);
+        Go(1.0, 1000);
     }
 
     @Override
